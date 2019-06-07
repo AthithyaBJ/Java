af73cd0ea3270ebd5f572ebc83a6df55fb0b3950
Athithya.B <athithyabalasubramani@gmail.com>
2019-06-07 08:35:24 +0530

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,11 +2,11 @@ public class Player {
    int number = 0;
      public void guess() {
      number = (int) (Math.random() * 10);
-     System.out.println(“I’m guessing “  + number);
+     System.out.println(â€œIâ€™m guessing â€œ  + number);
    }
  }
 public class GameLauncher {
-   public static void main (String[] args) {
+   public static void main (String args[]) {
       GuessGame game = new GuessGame();
       game.startGame();
    }
